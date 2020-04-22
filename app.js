f9543c3911abef7ee2088a4ceadf4069158dda41
dev09 <devdevdevdevdev09@gmail.com>
2020-04-22 22:20:26 +0900

    샘플 데이터 추가

---
+++ app.js
@@ -0,0 +1,26 @@
+// 멤버 리스트를 가져온다.
+const getMemberList = function(){
+
+};
+
+// json 파일 파싱
+const getJsonParse = function(fileName){
+
+}
+
+// 이전에 매칭이 되었는지 체크
+const checkLastWeek = function(member){
+
+}
+
+// 개인이 커밋확인할 사람 매칭
+const setCommitMember = function(list){
+    
+};
+
+const init = function(){
+
+};
+
+
+
