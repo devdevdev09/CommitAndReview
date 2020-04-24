574f28717802a4e3d2c8162c0818b6b2cd833197
dev09 <devdevdevdevdev09@gmail.com>
2020-04-25 02:16:48 +0900

    UPDATE READM.ME, data.json

--- app.js
+++ app.js
@@ -9,13 +9,29 @@ const getMemberList = function(){
 
 // json 파일 파싱
 const getJsonParse = function(fileName){
-    const data = jparser.getJsonData(fileName, "utf8");
+    const fileExt = "UTF8";
+
+    const data = jparser.getJsonData(fileName, fileExt);
+
     return data;
 }
 
 // 이전에 매칭이 되었는지 체크
 const checkLastWeek = function(member){
+    const data = getJsonParse("data.json");
 
+    const lastWeek = data.length;
+    const currentNo = data[lastWeek - 1].WEEK;
+
+    if(currentNo == 1){
+        // 1회차 일경우
+    }else{
+        // 1회차 이상일 경우
+    }
+
+    for(i in data){
+        console.log(data[0].DATA[i]);
+    }
 }
 
 // 개인이 커밋확인할 사람 매칭
@@ -27,8 +43,10 @@ const init = function(){
     const members = getMemberList();
 
     for(index in members){
-        console.log(members[index]);
+        console.log(members[index].USER_ID);
     }
+
+    checkLastWeek();
 };
 
 init();
