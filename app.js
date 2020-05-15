34053599141cfbfe69837ac1454b2ebf3536b4ad
dev09 <devdevdevdevdev09@gmail.com>
2020-05-15 14:44:35 +0900

    기간 및 회차 체크, 실제 파일에 데이터 저장

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const fs = require("fs");
 const fileName = "data.json";
 const fileExt = "utf8";
 
+// 프로젝트 순환주기 1번에 4주씩
+const PERIOD = 4;
+
 // 멤버 리스트를 가져온다.
 const getMemberList = function(){
     const members = getJsonParse("members.json");
@@ -12,7 +15,7 @@ const getMemberList = function(){
     return members;
 };
 
-const getDataList = function(){
+const getLastDataList = function(){
     const datas = getJsonParse("data.json");
 
     return datas;
@@ -40,7 +43,7 @@ const addDate = function(add, _date){
 
 // 이전에 매칭이 되었는지 체크
 const checkLastWeek = function(member){
-    const datas = getDataList();
+    const datas = getLastDataList();
 
     const len = datas.length;
     const currentLen = len - 1;
@@ -85,59 +88,37 @@ const setCommitMember = function(list){
     
 };
 
-const setJsonData = function(data){
-    const json = {
-        NO : data.NO,
-        WEEK : data.WEEK,
-        DATE : data.DATE,
+const setMatchData = function(){
+    const matchDataList = getMatchDataList();
+
+    const match_data_array = [];
+
+    let week = 0;
+    let no = 0;
+    
+    // 정해진 주기 보다 크면
+    if(getCurrentWeek() > PERIOD){
+        week = "1";
+        no = String(getCurrentNo());
+    }else{
+        no = String(getLastNo());
+        week = String(getCurrentWeek());
     }
+    
+    const matchData = {
+        NO : no,
+        WEEK : week,
+        DATE : getToday(),
+        DATA : matchDataList
+    };
+    
+    match_data_array.push(matchData);
+    match_data_array.push(matchData);
 
-    // TODO::유저 데이터 설정하기
-    const users = [
-            {
-                "USER_ID" : "1",
-                "COMMITS" : [
-                    {
-                        "NO" : "1",
-                        "USER_ID" : "2"
-                    },
-                    {
-                        "NO" : "2",
-                        "USER_ID" : "3"
-                    }
-                ]
-            },
-            {
-                "USER_ID" : "2",
-                "COMMITS" : [
-                    {
-                        "NO" : "1",
-                        "USER_ID" : "1"
-                    },
-                    {
-                        "NO" : "2",
-                        "USER_ID" : "3"
-                    }
-                ]
-            },
-            {
-                "USER_ID" : "3",
-                "COMMITS" : [
-                    {
-                        "NO" : "1",
-                        "USER_ID" : "2"
-                    },
-                    {
-                        "NO" : "2",
-                        "USER_ID" : "1"
-                    }
-                ]
-            }
-        ];
+    const lastData = getLastDataList();
+    lastData.push(matchData);
 
-    json.DATA = users;
-
-    return json;
+    return lastData;
 }
 
 // file update
@@ -184,24 +165,58 @@ const addExceptList = function(list, exept){
     
 }
 
+
+let LAST_DATA = [];
+const getLastData = function(){
+    if(LAST_DATA.length == 0){
+        LAST_DATA = getLastDataList();
+    }
+
+    const length = LAST_DATA.length;
+    const currentLen = length - 1;
+    const lastDate = LAST_DATA[currentLen].DATE;
+
+    const lastData = {
+        currentLen : currentLen,
+        week : LAST_DATA[currentLen].WEEK,
+        no : LAST_DATA[currentLen].NO,
+        date : LAST_DATA[currentLen].DATE,
+        checkDate : addDate(7, lastDate),
+        today : getToday(),
+    }
+
+    return lastData;
+}
+
+const getLastWeek = function(){
+    const lastData = getLastData();
+    return Number(lastData.week);
+}
+
+const getCurrentWeek = function(){
+    return getLastWeek() + 1;
+}
+
+const getLastNo = function(){
+    const lastData = getLastData();
+    return Number(lastData.no);        
+}
+
+const getCurrentNo = function(){
+    return getLastNo() + 1;
+}
+
 // 오늘이 마지막 데이터의 날짜보다 7일이상 지났는지 체크
 const checkMatchDate = function(){
-    const datas = getDataList();
 
-    const len = datas.length;
-    const currentLen = len - 1;
-    const currentWeek = datas[currentLen].WEEK;
-    const currentNo = datas[currentLen].NO;
-    const currentDate = datas[currentLen].DATE;
-    const checkDate = addDate(7, currentDate);
-    const today = getToday();
+    const lastData = getLastData();
 
-    const result = checkDate <= today;
+    const result = lastData.checkDate <= lastData.today;
 
     return result;
 }
 
-const init = function(){
+const getMatchDataList = function(){
     const memberList = getMemberList();
 
     let memberCount = memberList.length;
@@ -219,7 +234,7 @@ const init = function(){
         let reviews = [];
         for(let i = 0 ; i < REVIEW_COUNT_PER_USER; i++){
             let data = {};
-            data.NO = i+1;
+            data.NO = String(i+1);
             let random = getRandomInt(memberCount,1);
     
             reviewList[random - 1].REVIEW_COUNT -= 1;
@@ -246,18 +261,24 @@ const init = function(){
     
         reviewList.push.apply(reviewList, match_users);
     }
-    
-    for(i in dataList){
-        console.log(dataList[i]);
+
+    return dataList;
+}
+
+const init = function(){
+
+    if(checkMatchDate()){
+        console.log("기록 해야 됨");
+    }else{
+        console.log("기록 안해도 됨");
+        return;
     }
-    
+
+    const matchData = setMatchData();
+    fileWrite(JSON.stringify(matchData));
 };
 
-// init();
-
-
-console.log(checkMatchDate());
-
+init();
 
 
 
